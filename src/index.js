4834c5faa941956b96c60420d1d4b50466d57728
jungiang <jungiang@yahoo.com>
2019-03-26 17:39:19 -0700

    React Intro - Jun Giang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = ()=>{
+    const randomNumber = Math.floor(Math.random()*1000+1);
+    return randomNumber;
+}
+
+const user = {
+    name: 'Jun',
+    luckyNumber: luckyNumber()
+}
+
+const greeting = user=>(
+    <div className="container">
+        <h1>Welcome {user.name}</h1>
+        <h2 className="text-muted">Your lucky number is: {user.luckyNumber}</h2>
+    </div>
+)
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
