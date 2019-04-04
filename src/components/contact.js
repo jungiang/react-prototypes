cfe6a42b076c8b8bf2e8399299fb4c0510c6acc0
jungiang <jungiang@yahoo.com>
2019-04-03 18:39:14 -0700

    Feature sets 1-5 finished; move onto optionals

---
+++ src/components/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1>Contact</h1>
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, amet? Fugiat molestias facilis illo quas?</p>
+    </div>
+)
