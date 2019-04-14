ba48a152f12a3b77e82544aa6528430e5eda4059
Jun Giang <jungiang@yahoo.com>
2019-04-13 17:45:39 -0700

    issue after adding createStore and provider to index

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,9 @@
+import React from 'react';
 
-
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Redux Clock!</h1>
+        </div>
+    )
+}
---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,8 @@
+const DEFAULT_STATE = { time: null };
+
+export default function(state = DEFAULT_STATE, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
